2a7c4fb15c2b33bb5b571d2384ec679bae4be6bc
ccchen <ccchen9762@gmail.com>
2025-07-17 01:31:41 -0700

    update introduction; store and link resume in static

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -4,7 +4,6 @@ import { TypeAnimation } from 'react-type-animation';
 
 import OceanCanvas from './OceanCanvas.js'
 
-import links from '../../assets/data/Links.json'
 import bgVideo from '../../assets/bg.mp4'
 
 function Profile() {
@@ -28,7 +27,7 @@ function Profile() {
           <TypeAnimation
             sequence={[
               " ",
-              "Michael Chen's base",
+              "Michael Chen",
             ]}
             wrapper="span"
             speed={50}
@@ -37,22 +36,22 @@ function Profile() {
             cursor={false}
           />
         </Row>
-        <Row className="mt-4 px-4 w-full h-fit text-slate-50">
+        <Row className="mt-5 px-4 w-full h-fit text-slate-50">
           <motion.div
             className=""
             initial={{ opacity: 0, scale: 1 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
               duration: 1.2,
-              delay: 1.4,
+              delay: 1.0,
               ease: [0, 0.71, 0.2, 1.01]
             }}>
-            <p className="codeFont text-xl">Master of Science in Computer Science from USC.</p>
-            <p className="codeFont text-xl">Experienced in computer graphics, game, Android and web development.</p>
-            <p className="codeFont text-xl">Fond of music, gaming and traveling.</p>
+            <motion.p className="codeFont text-xl">A Passionate Full-stack engineer with a background in Master of Science in Computer Science from University of Southern California.</motion.p>
+            <motion.p className="codeFont text-xl">Expertise spans backend infrastructure (Docker, data integration, reliability) and responsive frontend development. </motion.p>
+            <motion.p className="codeFont text-xl">Enjoys exploring the creative challenges of computer graphics and game development.</motion.p>
           </motion.div>
         </Row>
-        <Row className="mt-4 px-4 w-full h-fit text-yellow-200">
+        {/*<Row className="mt-4 px-4 w-full h-fit text-yellow-200">
           <TypeAnimation
             sequence={[
               2000,
@@ -64,7 +63,7 @@ function Profile() {
             repeat={0}
             cursor={false}
           />
-        </Row>
+        </Row>*/}
         <Row className="mt-5 px-4 w-full h-16">
           <Col className="w-fit h-fit codeFont text-2xl">
             <motion.div
@@ -73,17 +72,18 @@ function Profile() {
             animate={{ opacity: 1, scale: 1 }}
             transition={{
               duration: 0.8,
-              delay: 3.2,
+              delay: 1.75,
               ease: [0, 0.71, 0.2, 1.01]
             }}>
               <Button 
-                href={links.links[0]} 
+                //href={links.links[0]} 
+                href={process.env.PUBLIC_URL + "/Resume/resume.pdf"}
                 target="_blank" 
                 style={{fontSize:"20px"}}
                 variant="warning"
                 className="w-fit h-fit px-3 py-2"
                 >
-                  My Resume
+                  Resume
               </Button>
             </motion.div>     
           </Col>
